b186d2a53b09deb120f9f7d3c4f702e3b073ce48
Zhuanglijuan <1330129868@qq.com>
2019-01-14 20:15:07 +0800

    基于用户业务的API修改

--- guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -4,5 +4,45 @@ package com.stylefeng.guns.api.user;
  * @Author: zhuanglj
  */
 public interface UserAPI {
+    /**
+     * 登陆
+     *
+     * @param username :用户名
+     * @param password :密码
+     * @return
+     */
     boolean login(String username, String password);
+
+    /**
+     * 注册
+     *
+     * @param userModel
+     * @return
+     */
+    boolean register(UserModel userModel);
+
+    /**
+     * 查询用户信息
+     *
+     * @param username
+     * @return
+     */
+    boolean checkUserName(String username);
+
+    /**
+     * 获取用户信息
+     *
+     * @param uuid
+     * @return
+     */
+    UserInfoModel getUserInfo(int uuid);
+
+    /**
+     * 更新用户信息
+     *
+     * @param userInfoModel
+     * @return
+     */
+    UserInfoModel updateUserInfo(UserInfoModel userInfoModel);
+
 }
